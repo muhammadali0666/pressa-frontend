0fd6b1081d17d910de4ada43257d84c4995a81e8
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2023-06-19 14:20:29 +0500

    updated

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,35 +1,68 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { Home } from '../pages/Home'
-import { BizHaqimizda } from '../pages/Bizhaqimizda'
-import { NotFound } from '../components/NotFound'
-import { Login } from '../components/Login'
-import { Register } from '../components/Register'
-import { ElonBerish } from '../pages/Elon'
-import { Admin } from '../pages/Admin'
-import { Accepted } from '../pages/Accepted'
-import { Canceled } from '../pages/cancelled'
-import { SinglePage } from '../pages/SinglePage'
-import { useState } from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { BizHaqimizda } from "../pages/Bizhaqimizda";
+import { NotFound } from "../components/NotFound";
+import { Login } from "../components/Login";
+import { Register } from "../components/Register";
+import { ElonBerish } from "../pages/Elon";
+import { Admin } from "../pages/Admin";
+import { Accepted } from "../pages/Accepted";
+import { Canceled } from "../pages/cancelled";
+import { SinglePage } from "../pages/SinglePage";
+import { useState } from "react";
 
 export const RouteWrapper = () => {
+  const [info, setInfo] = useState([]);
+  const [search, setSearch] = useState([]);
 
-  const [info, setInfo] = useState([])
-  const [search, setSearch] = useState([])
-  
+  const getToken = localStorage.getItem("token");
+
   return (
     <Routes>
-      <Route path='/home' element={<Home info={info} setInfo={setInfo}/>}/>
-      <Route path='/biz' element={<BizHaqimizda/>} />
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/' element={<Register/>}/>
-      <Route path='/elon' element={<ElonBerish/>}/>
-      <Route path='/admin' element={<Admin search={search} setSearch={setSearch}/>}/>
-      <Route path='/accepted' element={<Accepted search={search} setSearch={setSearch}/>}/>
-      <Route path='/waiting' element={<Admin search={search} setSearch={setSearch}/>}/>
-      <Route path='/canceled' element={<Canceled search={search} setSearch={setSearch}/>}/>
-      <Route path='/single' element={<SinglePage/>}/>
-      <Route path='*' element={<NotFound/>}/>
+      {getToken ? (
+        <Route path="/home" element={<Home info={info} setInfo={setInfo} />} />
+      ) : (
+        <Route path="/login" element={<Login />} />
+      )}
+      <Route path="/biz" element={getToken ? <BizHaqimizda /> : <Login />} />
+      <Route path="/login" element={getToken ? <Login /> : <Login />} />
+      <Route path="/" element={getToken ? <Register /> : <Login />} />
+      <Route path="/elon" element={getToken ? <ElonBerish /> : <Login />} />
+      {getToken ? (
+        <Route
+          path="/admin"
+          element={<Admin search={search} setSearch={setSearch} />}
+        />
+      ) : (
+        <Route path="/login" element={<Login />} />
+      )}
+      {getToken ? (
+        <Route
+          path="/accepted"
+          element={<Accepted search={search} setSearch={setSearch} />}
+        />
+      ) : (
+        <Route path="/login" element={<Login />} />
+      )}
+      {getToken ? (
+        <Route
+          path="/waiting"
+          element={<Admin search={search} setSearch={setSearch} />}
+        />
+      ) : (
+        <Route path="/login" element={<Login />} />
+      )}
+      {getToken ? (
+        <Route
+          path="/canceled"
+          element={<Canceled search={search} setSearch={setSearch} />}
+        />
+      ) : (
+        <Route path="/login" element={<Login />} />
+      )}
+      <Route path="/single" element={getToken ? <SinglePage /> : <Login />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
-  )
-}
+  );
+};
